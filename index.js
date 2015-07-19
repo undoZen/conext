5d1fa19a8e26a35ab7962c0783b5266f002f6302
Anson Yu Wang <opensource@undozen.com>
2015-07-19 21:08:48 +0800

    you dont need call next() at last now

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 'use strict';
 var co = require('co');
+function once(fn) {
+    var called = false;
+    return function () {
+        if (called) {
+            return;
+        }
+        called = true;
+        return fn.apply(this, arguments);
+    }
+}
 module.exports = function (gn) {
     var wrapped = co.wrap(gn);
     return function (req, res, next) {
-        wrapped.call(this, req, res, next).catch(next);
+        next = once(next);
+        wrapped.call(this, req, res, next).then(next).catch(next);
     };
 }
--- test/cases/node.js
+++ test/cases/node.js
@@ -8,9 +8,47 @@ var app = express();
 app.get('/ok', conext(function * (req, res, next) {
     res.end(yield Promise.resolve('ok'));
 }));
+var mid = conext(function *(req, res, next) {
+    res.hello = yield Promise.resolve('world');
+});
+var midWithNext = conext(function *(req, res, next) {
+    res.hello = yield Promise.resolve('undoZen');
+    next();
+});
+app.get('/mid', mid, conext(function * (req, res, next) {
+    res.end(yield Promise.resolve(res.hello));
+}));
+app.get('/midnext', midWithNext, conext(function * (req, res, next) {
+    res.end(yield Promise.resolve(res.hello));
+}));
+app.get('/throw', conext(function * (req, res, next) {
+    throw new Error('threw');
+}));
 app.get('/err', conext(function * (req, res, next) {
     return Promise.reject(new Error('errmsg'));
 }));
+
+tape(function (test) {
+    test.plan(2);
+    supertest(app)
+    .get('/mid')
+    .expect(200)
+    .end(function (err, response) {
+        test.ok(!err);
+        test.equal(response.text, 'world');
+    });
+});
+
+tape(function (test) {
+    test.plan(2);
+    supertest(app)
+    .get('/midnext')
+    .expect(200)
+    .end(function (err, response) {
+        test.ok(!err);
+        test.equal(response.text, 'undoZen');
+    });
+});
 
 tape(function (test) {
     test.plan(2);
@@ -31,5 +69,16 @@ tape(function (test) {
     .end(function (err, response) {
         test.ok(!err);
         test.ok(response.text.indexOf('Error: errmsg') > -1);
+    });
+});
+
+tape(function (test) {
+    test.plan(2);
+    supertest(app)
+    .get('/throw')
+    .expect(500)
+    .end(function (err, response) {
+        test.ok(!err);
+        test.ok(response.text.indexOf('Error: threw') > -1);
     });
 });
